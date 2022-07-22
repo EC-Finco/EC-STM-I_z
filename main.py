8d5898ec6bad3514df18b63028895b2bb8b6a68f
EC-Finco <matteo.finco.3@studenti.unipd.it>
2022-07-22 13:04:21 +0200

    Import and baseline command

--- main.py
+++ main.py
@@ -1,16 +1,74 @@
-# This is a sample Python script.
+#-----------------------------------MODULE CHECKS------------------------------
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Check for modules, try to exit gracefully if not found
+import sys
+import importlib
+
+import pybaselines
+
+try:
+    importlib.import_module('numpy')
+    foundnp = True
+except ImportError:
+    foundnp = False
+try:
+    importlib.import_module('matplotlib')
+    foundplot = True
+except ImportError:
+    foundplot = False
+try:
+    importlib.import_module('pandas')
+    foundpd = True
+except ImportError:
+    foundplot = False
+if not foundnp:
+    print("Numpy is required. Exiting")
+    sys.exit()
+if not foundplot:
+    print("Matplotlib is required. Exiting")
+    sys.exit()
+if not foundpd:
+    print("Pandas is required. Exiting")
+    sys.exit()
+
+#-------------------------------------------------------------------------------
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import pandas as pd
+import math
+from pybaselines.whittaker import asls
+#-------------------------------------------------------------------------------
+
+import os
+import glob
+
+# Stop message from appearing
+import warnings
+warnings.filterwarnings("ignore",".*GUI is implemented.*")
+warnings.filterwarnings("ignore",".*No labelled objects found.*")
+
+#importing filenames
+extension = 'ts'
+#insert the path of the spectra
+path=input("Type the path of spectra: ")
+os.chdir(path)
+#uncomment to check that cwd is the wanted one
+#path = os.getcwd()
+#print("The targeted path is", path)
+#Find relevant TSs in folder
+tsresult = [i for i in glob.glob('*.{}'.format(extension))]
+#uncomment to Verify the content of tsresult
+#print("Plotting the following:")
+#print(tsresult)
+
+#import, baseline and export
+k=0
+#while k < len(tsresult):
+path = os.getcwd()
+path = path + "/" + tsresult[1]
+spectra=pd.read_csv(path, delimiter=" ",header=13, names=["Position","Current"],engine='python', skipfooter=1)
+baseline=pybaselines.whittaker.asls(spectra.Current)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
